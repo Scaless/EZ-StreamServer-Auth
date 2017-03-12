3e6b940635eb66d4c602332a64196fd42758f132
Scales <scales@scal.es>
2017-03-12 03:59:00 -0400

    Initial stream authentication TODO: add database and dynamic authentication

--- ezstream-auth.go
+++ ezstream-auth.go
@@ -3,13 +3,35 @@ package main
 import (
     "fmt"
     "net/http"
+    "strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func on_publish(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    fmt.Printf("Publish Started: %s\n", r.FormValue("name"))
+    secretkey := r.FormValue("swfurl")[strings.Index(r.FormValue("swfurl"), "?")+1:]
+    fmt.Printf("secret: %s\n", secretkey)
+    if(r.FormValue("name") == "scalesstreamkey" && secretkey == "123456"){
+        w.WriteHeader(http.StatusOK)
+    } else {
+        w.WriteHeader(http.StatusUnauthorized)
+    }
+}
+
+func on_publish_done(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    fmt.Printf("Publish Stopped: %s\n", r.FormValue("name"))
+}
+
+func on_play(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    fmt.Printf("asdf")
+    //fmt.Printf("Stream Playing: %s\n", r.FormValue("name"))
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/on_publish", on_publish)
+    http.HandleFunc("/on_publish_done", on_publish_done)
+    http.HandleFunc("/on_play", on_play)
     http.ListenAndServe(":8080", nil)
 }
